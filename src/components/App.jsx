b81d9f3535776b0c5e40588d422d748b7326d8cd
YevhenBez <byxgalter@bigmir.net>
2023-10-26 13:29:07 +0300

    Создал   container   и   box    Тестовое для  Аbyss
    Создал   container   и   box   согласно видоса   https://www.youtube.com/watch?v=ZcDWyVj6-uU    При этом обрати внимание, что данный шаблон (болванка) репо у меня без настроек для    TypeScript   Поэтому я немного отклоняюсь от кода видоса   https://www.youtube.com/watch?v=ZcDWyVj6-uU    просто для того, чтобы у меня хотя бы работало. А именно, в видосе не используют файл   App.jsx   там используют файл   App.tsx     Также обрати внимание, что в моём файле   App.jsx    стили    className    пока фейковые (сами стили не написаны пока). Также обрати внимание, т.к. я переделал переменную  Арр на функцию   Арр   и экспортнул её при помощи   export default   то в файле  index.js   я убрал скобки при импорте  Арр    Тестовое для  Аbyss

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,11 @@
-export const App = () => {
+function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <main>
+      <div className="container">
+        <div className="box">Этот текст - пока тренировка</div>
+      </div>
+    </main>
   );
 };
+
+export default App;
